99a65d476653eb5c93928ad507627c6bda3b6c18
DanilTkachyov <90649118+DanilTkachyov@users.noreply.github.com>
2022-05-16 05:04:52 +0300

    Update form.php

--- 6/form.php
+++ 6/form.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8" />
     <link rel="stylesheet" href="./style.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
-    <title>Task 5 Tkachev</title>
+    <title>Task 6 Tkachev</title>
 </head>
 
 <body>
